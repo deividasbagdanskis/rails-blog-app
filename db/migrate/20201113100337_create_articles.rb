716725647b715421d6143cdf5ca35a3929c9ae93
Deividas Bagdanskis <51085446+deividasbagdanskis@users.noreply.github.com>
2020-11-13 12:25:56 +0200

    Create Article model and migration

---
+++ db/migrate/20201113100337_create_articles.rb
@@ -0,0 +1,13 @@
+class CreateArticles < ActiveRecord::Migration[6.0]
+  def change
+    create_table :articles do |t|
+      t.string :title
+      t.string :author
+      t.string :image
+      t.string :excerpt
+      t.text :text
+
+      t.timestamps
+    end
+  end
+end
