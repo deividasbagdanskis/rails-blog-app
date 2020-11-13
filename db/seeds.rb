303021f948a70b604f6c1d942cd23f2cd46f448e
Deividas Bagdanskis <51085446+deividasbagdanskis@users.noreply.github.com>
2020-11-13 20:17:29 +0200

    Seed second article to db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Article.destroy_all
+
 Article.create!([{
   title: "Three Ways to Have a More Secure Online Experience",
   author: "Joe Doe",
@@ -75,4 +78,82 @@ Article.create!([{
     providers. You can even choose multiple VPNs so that Orchid periodically
     jumps from one provider to the next, so no singular service has the entire
     picture of your online activities."
+},
+{
+  title: "7 Best Examples of Educational Technologies ",
+  author: "Joe Doe",
+  image: "edu-tech-min.png",
+  excerpt: "Since technology advances by the day, keeping up with innovations
+  in the classroom can seem daunting.",
+  text: "Since technology advances by the day, keeping up with innovations in
+    the classroom can seem daunting. Each day, new technologies meant for both
+    students and educators are developed. And without hesitation, they are
+    quickly piloted or adopted by schools on a full-scale basis.\n\nMeanwhile,
+    the majority of stakeholders in education continue to appreciate the value
+    of technology. From the moment these two concepts merged, students have
+    reported better grades, and teachers have an easier time managing their
+    routine responsibilities. As we go into the future, we can be confident
+    that tech will make things only better as far as education is concerned.
+    \n\nThis article focuses on seven examples of innovative educational
+    technologies that are making their way into the classroom. Have a look.\n\n
+    1. 3D Printing\n\n3D printing (prototyping) enables scholars to learn
+    better. It allows them to produce tangible/physical prototypes for
+    science, art, biology, engineering, and other lessons. With these items,
+    students can better comprehend how things work, their design, and much
+    more. This technology comes with several advantages, but here, we
+    highlight two of them. The first one is student curiosity. With 3D
+    printing, learners can convert their imaginations into reality, satisfying
+    both their creativity and curiosity. Another advantage of this technology
+    is time-saving. Since educators do not have to spend lots of hours
+    developing models for class (3D printers will do that), they can save
+    time. This way, they can focus on other aspects of their job, making the
+    learning process more productive.\n\n2. Virtual & Augmented Learning
+    Experiences\n\nVirtual Reality (VR) and Augmented Reality (AR) are an
+    excellent and welcome technological addition to education. With these
+    solutions, students gain an immersive learning experience without the need
+    to move a lot. For example, students learning about space can watch a 3D
+    film on VR devices that give them sensations of floating in the galaxy.
+    Better yet, educators can offer a background voice to explain various
+    things in space. For students studying medical disciplines, these
+    technologies can offer more practical experience for surgeries. By doing
+    this, students get to understand theoretical concepts better.\n\n3. Video
+    Conferencing\n\nThanks to the high-speed Internet, it is now possible to
+    link lecture rooms in different places and participate in the same
+    classes. This approach has since made it easy for learners and educators
+    to work together no matter their destinations on the globe.\n\nMoreover,
+    this technology has helped cut down on the number of missed classes. If a
+    teacher or professor is not close to school, they could video conference
+    with students during class hours. Still, this technology has made it
+    possible for students and educators to organize face-to-face sessions with
+    parents and other stakeholders.\n\n4. Gamification\n\nGames are a more fun
+    way to learn. Thanks to technology, students can now access various
+    educational games during class or even at home. According to research,
+    challenge-based gamification may improve learners' performance by up to
+    89.45% compared to only being present in class.\n\n5. Cloud Computing\n\n
+    This technology has perhaps done a lot for high tech education than any
+    other. With cloud computing, people can access information whenever and
+    from wherever. This has made it possible for students and teachers to
+    access courses and other educational resources conveniently.\n\nToday, a
+    learner can keep reading a book and cooperate with their peers/educators
+    on the go. Better yet, they no longer need to acquire massive drives to
+    store their educational material. These resources can be stored in and
+    accessed from the cloud whenever and at any location.\n\n6. Tablets\n\n
+    This educational technology has enabled students to access digital course
+    material, boosting engagement via mixed media. Also, since learners do not
+    have to buy physical books, this technology has helped students and
+    parents save money.\n\nBesides, given that modern-day students are way
+    more tech-savvy, using tablets during classes makes their learning easier.
+    Importantly, these devices allow learners to access their institution's
+    learning management system and other useful academic help services. On
+    such educational platforms, a student can buy essay or get advice on
+    handling various assignments.\n\n7. Artificial Intelligence (AI)\n\nThis
+    technology has found its way into the education sector in different ways.
+    For one, it has led to the automation of tasks such as student grading,
+    streamlining of administrative responsibilities, and individualized
+    learning.\n\nFinal Words\n\nEducational establishments have more
+    technological solutions at their disposal than ever before. It is up to
+    them to gauge their resources to determine the best tools for their
+    students and educators. Already, studies show that tech can enhance the
+    learning process and yield impressive outcomes. Importantly, in this age,
+    incorporating tech into the classroom is not a luxury but a necessity."
 }])
