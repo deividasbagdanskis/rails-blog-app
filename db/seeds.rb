e89b98f3a8856e8955963a9c6ec8d131724a063b
Deividas Bagdanskis <51085446+deividasbagdanskis@users.noreply.github.com>
2020-11-13 16:03:49 +0200

    Seed article to db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,74 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Article.create!([{
+  title: "Three Ways to Have a More Secure Online Experience",
+  author: "Joe Doe",
+  image: "online-security.png",
+  excerpt: "Criminals have gotten more and more clever when it comes to their
+    ability to track and hack.",
+  text: "Criminals have gotten more and more clever when it comes to their
+    ability to track and hack.\n\nBut it isn’t just criminals you have to worry
+     about. Sure, they can use cookies and pixels and other tools to track your
+     online movements looking for a weakness or for you to make a mistake and
+    expose yourself (like clicking a phishing link), but what about marketers,
+    governments, and even your own internet service provider? How do you hide
+    your information and online presence from your own internet company?\n\n
+    Let’s look at a few ways that you can protect your identity and secure your
+     online transactions.\n\nTor Browser\n\nTor is one of the easiest ways to
+    protect yourself online. Just like you can use Google to download Mozilla
+    Firefox or Opera, you can search ‘download Tor’ and safely add the browser
+    to your desktop.\n\nBut what is Tor?\n\nThe Tor project, originally called
+    The Onion Router, is open-source software that enables anonymous online
+    communication. If you’re reading this and thinking some slightly nefarious
+    thoughts about hacking your school’s computers to change your grades or
+    hacking your office to play a prank … it is not that secure—at least not
+    in its basic stock form. That said, you can adjust your Tor settings, run
+    it off a segmented virtual machine, and then couple it with VPNs, and
+    you’ll be safe against marketers, criminals, and even your own ISP!\n\n
+    How does it work?\n\nIt’s like an onion. Hence, The Onion Router. It has
+    nested application layers that are encrypted (like an onion). Basically,
+    Tor encrypts your data and the IP address multiple times and sends it
+    through a series of random Tor relays. The relays each decipher a piece of
+    the encryption before passing it on to the next relay for further
+    decoding. This makes it extremely difficult for any network surveillance
+    to determine the source and destination.\n\nBitCoin\n\nYou may be
+    wondering why Bitcoin will secure your transactions and protect your
+    identity. Well, just as we stated above, marketers sometimes sell info.
+    But often, it’s not even them. Even though they have taken steps to secure
+    your information against criminals, the internet service provider may be
+    looking over their shoulders. If you are using Tor browser and handle your
+    purchases in BTC, it’s nearly impossible for an opportunistic hack or
+    identity theft to occur.\n\nBitcoin is not nearly as anonymous as people
+    once thought; you have to purchase it after all. But you can look at it
+    similarly to replacing all of that information you fill out when making an
+    online purchase, bank card number, billing address, name, etc. with just
+    an email address. With BTC, the blockchain verifies the transaction, so
+    you don’t need to go through the normal, archaic process of verifying your
+    identity with banks talking back and forth to one another. If the website
+    you are purchasing from does end up getting scraped for information, the
+    most potential criminals will get your BTC address. They can’t do much with
+    it … it’s like someone having your email address, but much more secure.
+    The most they can do is send funds to your address, which wouldn’t be too
+    bad!\n\nBitcoin is used by many online outlets due to Standard wire
+    transfers and foreign purchases typically involve fees and exchange costs.
+    Since bitcoin transactions have no intermediary institutions or government
+    involvement, in terms of the costs of transacting are kept very low. This
+    can be a major advantage for travelers, also in terms of accessibility
+    users are able to send and receive bitcoins with only a smartphone or
+    computer, making it very attractive to many types of industries like
+    Microsoft, AT&T, Fast Food chain restaurants, Airlines, the gambling
+    industry, Gift card online retailers even Amazon (though not directly).\n\n
+    Orchid\n\nOrchid is a great way to secure your online activities while on
+    your mobile devices. It’s a crypto-powered VPN that operates on the
+    Ethereum network. You can use ETH to buy a few Orchid, which is used to
+    purchase affordable prepaid VPN services throughout the Orchid VPN app.
+    The coolest part is that you have complete control, and there is no
+    singular VPN provider—like when you buy a monthly service
+    subscription—that could in turn and sell your information.\n\nEssentially,
+    you log on to Orchid, and it searches for the most viable bandwidth and
+    rents it for you so that your sessions are split between different
+    providers. You can even choose multiple VPNs so that Orchid periodically
+    jumps from one provider to the next, so no singular service has the entire
+    picture of your online activities."
+}])
