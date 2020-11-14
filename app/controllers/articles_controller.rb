a8c164268259f53fddd4a6f2384958c3c23edd7b
Deividas Bagdanskis <51085446+deividasbagdanskis@users.noreply.github.com>
2020-11-14 15:56:05 +0200

    Modify articles show to display article from db

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,11 @@
+# frozen_string_literal: true
+
 class ArticlesController < ApplicationController
   def index
     @articles = Article.all
   end
 
   def show
-    render "secure_online_experience"
+    @article = Article.find(params[:id])
   end
 end
