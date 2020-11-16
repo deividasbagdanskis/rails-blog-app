a44577a1bb431894003c25b20e8311b0935a00f3
Deividas Bagdanskis <51085446+deividasbagdanskis@users.noreply.github.com>
2020-11-16 17:28:36 +0200

    Create login view

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,7 @@ Rails.application.routes.draw do
   # sets pages home view as root
   root 'pages#home'
 
-  resources :articles
-  # redirects to pages/about view
+  resources :articles # redirects to pages/about view
   get 'about', to: 'pages#about'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'login', to: 'pages#login' # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
