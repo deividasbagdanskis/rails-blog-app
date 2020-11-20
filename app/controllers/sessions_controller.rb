8d50cafddd15dba29439f82b26b01063a2f4663d
Deividas Bagdanskis <51085446+deividasbagdanskis@users.noreply.github.com>
2020-11-20 21:40:40 +0200

    Display log in error message

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,14 +5,15 @@ class SessionsController < ApplicationController
 
   def create
     @request_user = User.new(user_params)
+
     user = User.find_by(email: @request_user.email)
-    # logger.debug "User password: #{@request_user.password_digest}"
+
     if user && user.authenticate(@request_user.password)
       session[:user_id] = user.id
       redirect_to root_path
     else
-      flash[:alert] = 'Email or password is invalid'
-      render 'new'
+      flash[:error] = 'Email or password is invalid'
+      redirect_to action: :new
     end
   end
 
