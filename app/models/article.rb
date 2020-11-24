a5a5901358195ee39c067b3e7b89bd4d8a6f8f76
Deividas Bagdanskis <51085446+deividasbagdanskis@users.noreply.github.com>
2020-11-24 16:12:09 +0200

    Add article validation

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,7 @@
 class Article < ApplicationRecord
   has_one_attached :image_file
+  validates :title, presence: true, length: { in: 10..85 }
+  validates :author, presence: true, length: { minimum: 5 }
+  validates :excerpt, presence: true, length: { in: 15..175 }
+  validates :text, presence: true
 end
