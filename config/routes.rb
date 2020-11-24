0c396eaeb893451cb0c7eedd05cca2ee40863182
Deividas Bagdanskis <51085446+deividasbagdanskis@users.noreply.github.com>
2020-11-24 21:14:20 +0200

    Create ability to delete articles

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
-  # root 'pages#home' # sets pages home view as root
-  root :to => 'pages#home'
+  root :to => 'pages#home' # sets pages home view as root
   resources :articles # redirects to pages/about view
-  # resources :users
   resources :sessions, only: %i[new create destroy]
 
   get 'about', to: 'pages#about'
