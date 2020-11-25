04ba4f17fe7ebfe8bf1de8a2702a59bb2a2148ea
Deividas Bagdanskis <51085446+deividasbagdanskis@users.noreply.github.com>
2020-11-25 20:04:43 +0200

    Create ability to search for articles

--- app/models/article.rb
+++ app/models/article.rb
@@ -5,4 +5,12 @@ class Article < ApplicationRecord
   validates :excerpt, presence: true, length: { in: 15..175 }
   validates :text, presence: true
   validates :image_file, presence: true
+
+  def self.search(search)
+    if search
+      self.all.where('lower(title) LIKE ?', "%#{search}%")
+    else
+      self.all
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   root :to => 'pages#home' # sets pages home view as root
-  resources :articles # redirects to pages/about view
+  resources :articles
   resources :sessions, only: %i[new create destroy]
 
-  get 'about', to: 'pages#about'
+  get 'about', to: 'pages#about' # redirects to pages/about view
   get 'login', to: 'sessions#new', as: 'login'
   get 'logout', to: 'sessions#destroy', as: 'logout'
 
