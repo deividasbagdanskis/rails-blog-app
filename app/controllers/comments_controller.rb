f46ff143e016044163942b0e09de2abbc796fb40
Deividas Bagdanskis <51085446+deividasbagdanskis@users.noreply.github.com>
2020-12-21 21:08:44 +0200

    Create ability to delete comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,6 @@
 class CommentsController < ApplicationController
+  before_action :require_login
+
   def create
     @article = Article.find(params[:article_id])
     @comment = Comment.new(comment_params)
@@ -11,9 +13,25 @@ class CommentsController < ApplicationController
     redirect_to article_path(@article)
   end
 
+  def destroy
+    article = Article.find(params[:article_id])
+
+    @comment = article.comments.find(params[:id])
+    @comment.destroy
+
+    redirect_to article_path(article)
+  end
+
   private
 
   def comment_params
     params.require(:comment).permit(:body)
+  end
+
+  def require_login
+    if current_user == nil
+      render plain: "You must be log in to access this section",
+          status: 403
+    end
   end
 end
