725f7042ea9e33b2a13a9822aac0c9403388fb8b
Deividas Bagdanskis <51085446+deividasbagdanskis@users.noreply.github.com>
2020-12-21 16:14:31 +0200

    Create create action in Comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,19 @@
 class CommentsController < ApplicationController
+  def create
+    @article = Article.find(params[:article_id])
+    @comment = Comment.new(comment_params)
+    @comment.article = @article
+    @comment.user = current_user
+    @comment.username = current_user_username
+
+    @comment.save
+
+    redirect_to article_path(@article)
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:body)
+  end
 end
